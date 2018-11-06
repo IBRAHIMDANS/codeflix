0e16194ec005baa94a873befee047cdfc2c61202
ibrahimdans <hibrahim69@live.fr>
2018-11-06 17:10:57 +0100

    d

--- onecode/season.04/e03.js
+++ onecode/season.04/e03.js
@@ -1,11 +1,35 @@
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
+const path = require ('path');
+const http = require ('http');
+const https = require ('https');
 
-const port = process.argv[2]
+let port = process.argv[2];
 
-if (port == undefined ) {
-   port = 1010
-   console.log(`Server is running at port ${port}`);
+if (port == undefined)
+{
+port = 1717;
+  console.log(`Server is running at port ${port}`);
+
+  http.createServer(function (req, res) {
+
+    const request = https.get(`https://www.reddit.com/r/perfectloops.json`, (resp) => {
+    let body = "";
+    resp.on('data', data => {
+        body += data.toString();
+    });
+    resp.on('end', () => {
+        let json = JSON.parse(body);
+        let string = ""
+        for(let i = 1; i < 11; i++) {
+              let gifUrl = json.data.children[i].data.url;
+              gifUrl = gifUrl.replace('.gifv','.gif')
+              string += `<div>
+                          <img src = "${gifUrl}" >
+                          </div>`
+              res.writeHeader(200, {"Content-Type": "text/html"});
+        }
+        res.write(string);
+        res.end()
+      });
+    });
+  }).listen(port);
 }
