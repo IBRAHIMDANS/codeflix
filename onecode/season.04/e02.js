94870e2c1a384e8745100e43c9e51d3bc8b66e5c
ibrahimdans <hibrahim69@live.fr>
2018-11-06 12:44:24 +0100

    d

--- onecode/season.04/e02.js
+++ onecode/season.04/e02.js
@@ -1,21 +1,31 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const path = require('path');
 
 const hostname = "127.0.0.1"
 const port = process.argv[2];
 
 if (port== undefined|| port !== 1337 ) {const port = 1337;}
 const serv = http.createServer((req,res) => {
-    res.statusCode = 200;
-    const q = url.parse(req.url,true);
-    let query = q.query;
-    let keys= Object.keys(query);
-    let values= Object.values(query);
-    let file
+  res.statusCode = 200;
+  const q = url.parse(req.url,true);
+  const path = q.pathname;
+  res.on('data', (data) => {
+    console.log("op");
+});
 
+  if (path === '/messages'){
+    let pathh = path.slice(1)
+    const value = { "messages" : []}
+    let d = new Date;
+    res.on
+    value.messages.push({"text" : data,"date" : d.toJSON()})
+    let file = fs.writeFileSync('text.json',JSON.stringify(value, null, 3))
+    let read = fs.readFileSync(text.json, 'utf8')
+  }
 
-    return res.end('done');
+  return res.end('done');
 })
 serv.listen(port, hostname, () => {
   console.log(`Server running at port : 1337`);
